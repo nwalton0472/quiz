4741add561860f62e994691f4c41ecd8c71057fb
Neota Walton <neota1951@gmail.com>
2019-01-17 23:25:27 +0000

    great

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /* global $ */
+var totalScore = 0;
 
 $(document).ready(function() {
     $("button").click(function() {
@@ -9,19 +10,25 @@ $(document).ready(function() {
         var q2Input = $("#question2").val();
         var q3Input = $("#question3").val();
         findScore1(q1Input);
-        var totalScore;
+        
     });
 
 });
 
 function findScore1(answer) {
+    if (answer ==="Winter"){
+        totalScore = totalScore +1;
+    } else if (answer==="Summer")
+    totalScore = totalScore + 2;
+}
+
+
+function findScore2(answer) {
+    if (answer ==="The Adventerous One"){
+        totalScore = totalScore + 4;
+    }
+}
+
+function findScore3(answer) {
     console.log(answer);
-}
-
-function findScore2(answer){
-    
-}
-
-function findScore3(answer){
-    
 }
